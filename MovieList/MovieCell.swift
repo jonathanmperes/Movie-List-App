c54a749da31234ee0906e63ee351d1982be587ba
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-02-21 20:47:03 -0300

    MovieListScreenViewControler and MovieCell added to the project

---
+++ MovieList/MovieCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieCell.swift
+//  MovieList
+//
+//  Created by Jonathan Marcelo Peres on 21/02/23.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var movieDescriptionLabel: UILabel!
+    
+}
