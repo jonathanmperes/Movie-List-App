b49433c8986dc9d96c06489241ec16c56be3bad6
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-02-24 16:31:14 -0300

    TableViewMovieCell created

---
+++ MovieList/TableViewMovieCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewMovieCell.swift
+//  MovieList
+//
+//  Created by Jonathan Marcelo Peres on 24/02/23.
+//
+
+import UIKit
+
+class TableViewMovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
