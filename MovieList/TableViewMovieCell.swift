36533793610f28e982bfb501ae237028b2c38317
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-02-27 15:48:40 -0300

    binding TableViewMovieCell elements

--- MovieList/TableViewMovieCell.swift
+++ MovieList/TableViewMovieCell.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 class TableViewMovieCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    
+    @IBOutlet weak var imageViewMovieCell: UIImageView!
+    @IBOutlet weak var labelTitleMovieCell: UILabel!
+    @IBOutlet weak var labelDescriptionMovieCell: UILabel!
+    //    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
     
 }
