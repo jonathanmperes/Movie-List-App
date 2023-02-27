c438b7e5a37fdfaefc1bffe5e18f878297165ee5
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-02-27 15:52:08 -0300

    deletingboilerplate code from TableViewMovieCell

--- MovieList/TableViewMovieCell.swift
+++ MovieList/TableViewMovieCell.swift
@@ -12,15 +12,4 @@ class TableViewMovieCell: UITableViewCell {
     @IBOutlet weak var imageViewMovieCell: UIImageView!
     @IBOutlet weak var labelTitleMovieCell: UILabel!
     @IBOutlet weak var labelDescriptionMovieCell: UILabel!
-    //    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
-    
 }
